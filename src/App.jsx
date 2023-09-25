138a2074a25b11412503298d6e822013eb17f3d4
CypherAlchemist <cypheralchemist@gmail.com>
2023-09-25 21:39:19 +0100

    dockerizing a react application and multistage build prod/dev with nginx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        {" "}
+        A simple hello world React App for containerization purposes with Docker
+        & Nginx
+      </div>
+      <div>cypherAlchemist !</div>
+    </>
+  );
+}
+
+export default App;
