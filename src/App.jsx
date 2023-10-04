19340a2135cbdbb954a0c81dce9862dd027b7c10
Tamimi Mohammed (cyb3rdust) <142602422+cyb3rdust@users.noreply.github.com>
2023-10-04 19:08:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
         A simple hello world React App for containerization purposes with Docker
         & Nginx
       </div>
-      <div>cypherAlchemist !</div>
+      <div> cyb3rdust </div>
     </>
   );
 }
